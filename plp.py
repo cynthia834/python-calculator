689e32d023a8d0be79787e1c06c46aab226d11ec
cynthia834 <akinyicynthia877@gmail.com>
2025-03-28 01:26:38 +0300

    python assignment submission week one

--- plp.py
+++ plp.py
@@ -1,27 +1,20 @@
-def calculator():
-    try:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        operation = input("Enter the operation (+, -, *, /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
 
-        if operation == '+':
-            result = num1 + num2
-        elif operation == '-':
-            result = num1 - num2
-        elif operation == '*':
-            result = num1 * num2
-        elif operation == '/':
-            if num2 == 0:
-                print("Error: Division by zero is not allowed.")
-                return
-            result = num1 / num2
-        else:
-            print("Invalid operation. Please enter one of +, -, *, or /.")
-            return
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        result = num1 / num2
+else:
+    print("Invalid operation. Please enter one of +, -, *, or /.")
 
-        print(f"Result: {result}")
-    except ValueError:
-        print("Invalid input. Please enter numerical values.")
-
-
-calculator()
+if operation in ['+', '-', '*', '/'] and not (operation == '/' and num2 == 0):
+    print(f"Result: {result}")
